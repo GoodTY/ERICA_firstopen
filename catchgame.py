4e7eb6b642f6c85a13a221aa0111e0ddf15ad16a
GoodTY <taek3153@gmail.com>
2019-12-05 14:32:32 +0900

    게임 화면 만들기

---
+++ catchgame.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400 
+
+pygame.init()
+
+scrren = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	
+time.sleep()
+while True:
+	pass
