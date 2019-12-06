0b9b667cdef78292000b1fc274803da3ec398393
GoodTY <taek3153@gmail.com>
2019-12-06 14:45:22 +0900

    흰 기본 화면 설정

--- catchgame.py
+++ catchgame.py
@@ -1,14 +1,31 @@
 import pygame
-import time
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 512 
 WHITE = (255, 255, 255)
 
+def Play_Game():
+	global screen, clock
+
+	crashed = False
+	while not crashed:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				crashed =True
+
+		screen.fill(WHITE)
+		pygame.display.update()
+		clock.tick(60)
+	pygame.quit()	
+
 def Start_Game():
 	global screen , clock
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	pygame.dispaly.setcaption('Catching')
+	pygame.display.set_caption('Catching')
 	clock = pygame.time.Clock()
-	Playing_Game()
+	Play_Game()
+def main():
+	Start_Game()
+
+main()
