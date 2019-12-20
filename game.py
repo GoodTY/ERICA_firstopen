8b4907551cd33e6719740480e691fde442a14cde
GoodTY <taek3153@gmail.com>
2019-12-20 16:47:06 +0900

    바구니 생성

--- game.py
+++ game.py
@@ -11,13 +11,24 @@ pygame.display.set_caption("Catching Bananas")
 icon = pygame.image.load('monkey.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('basket.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x,y):
+	screen.blit(playerImg, (x, y))
 # Game Loop
 running = True
 while running:
+
+	# 빨강 초록 파랑
+	screen.fill((0,0,0))
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 
-	# RGB
-	screen.fill((255,0,0))
+	player()
 	pygame.display.update()
