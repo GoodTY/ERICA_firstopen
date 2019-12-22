0756e948521304fb1eb993019c1d811fc7adb6a8
GoodTY <taek3153@gmail.com>
2019-12-22 13:25:56 +0900

    충돌 구현

--- game.py
+++ game.py
@@ -1,11 +1,10 @@
 import pygame
 import random
-
+import math
 
 pygame.init()
 
 #create the screen
-
 screen = pygame.display.set_mode((800, 600))
 
 #background Image
@@ -26,7 +25,8 @@ playerX_change = 0
 enemyImg = pygame.image.load('banana.png')
 enemyX = random.randint(0, 800) 
 enemyY = 0
-enemyY_change = 0.3
+enemyY_change = 5
+
 
 def player(x,y):
 	screen.blit(playerImg, (x, y))
@@ -34,10 +34,17 @@ def player(x,y):
 def enemy(x, y):
 	screen.blit(enemyImg, (x, y))
 
+def isCollision(enemyX, enemyY, playerX, playerY):
+	distance = math.sqrt((math.pow(enemyX-playerX,2)) + (math.pow(enemyY - playerY,2)))
+	
+	if distance < 27:
+		return True
+	else:
+		return False
+
 # Game Loop
 running = True
 while running:
-
 	# 빨강 초록 파랑
 	screen.fill((0,0,0))
 	screen.blit(background, (0,0))
@@ -47,14 +54,14 @@ while running:
 		# if keastroke is pressed check whether its right or left
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
-				playerX_change = 0.2
+				playerX_change = 10
 			if event.key == pygame.K_LEFT:
-				playerX_change = -0.2
+				playerX_change = -10
 		if event.type == pygame.KEYUP:
 			if event.type == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 				playerX_change = 0
 
-	# Enemy Movement
+	# Player Movement
 	playerX += playerX_change
 
 	if playerX <= 0:
@@ -69,6 +76,14 @@ while running:
 		enemyY_change = -0.3
 		enemyY += enemyY_change
 
+	# Collision
+	collision = isCollision(enemyX, enemyY, playerX, playerY)
+	if collision:
+		print(score)
+		enemyX = random.randint(0, 735)
+		enemyY = 0
+
 	player(playerX, playerY)
 	enemy(enemyX, enemyY)
+
 	pygame.display.update()
