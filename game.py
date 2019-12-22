c9cd70053207a0e31962108b85bad337ef566a5f
GoodTY <taek3153@gmail.com>
2019-12-22 15:59:45 +0900

    속도 변환 및 명칭 수정 enemy -> banana

--- game.py
+++ game.py
@@ -36,28 +36,28 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
-# Enemy
-enemyImg = []
-enemyX = []
-enemyY = []
-enemyY_change = []
-num_of_enemies = random.randint(2,10)
+# Banana
+bananaImg = []
+bananaX = []
+bananaY = []
+bananaY_change = []
+num_of_bananas = random.randint(2,5)
 
-for i in range(num_of_enemies):
-	enemyImg.append(pygame.image.load('banana.png'))
-	enemyX.append(random.randint(0, 736)) 
-	enemyY.append(0)
-	enemyY_change.append(20)
+for i in range(num_of_bananas):
+	bananaImg.append(pygame.image.load('banana.png'))
+	bananaX.append(random.randint(0, 736)) 
+	bananaY.append(random.randint(0,20))
+	bananaY_change.append(5)
 
 
-def player(x,y):
+def player(x, y):
 	screen.blit(playerImg, (x, y))
 
-def enemy(x, y, i):
-	screen.blit(enemyImg[i], (x, y))
+def banana(x, y, i):
+	screen.blit(bananaImg[i], (x, y))
 
-def isCollision(enemyX, enemyY, playerX, playerY):
-	distance = math.sqrt((math.pow(enemyX-playerX,2)) + (math.pow(enemyY - playerY,2)))
+def isCollision(bananaX, bananaY, playerX, playerY):
+	distance = math.sqrt((math.pow(bananaX-playerX,2)) + (math.pow(bananaY - playerY,2)))
 	
 	if distance < 27:
 		return True
@@ -85,9 +85,9 @@ while running:
 		# if keastroke is pressed check whether its right or left
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
-				playerX_change = 10
+				playerX_change = 15
 			if event.key == pygame.K_LEFT:
-				playerX_change = -10
+				playerX_change = -15
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_RIGHT:
 				playerX_change = 0
@@ -102,26 +102,26 @@ while running:
 		playerX = 736
 
 	# Enemy Movement
-	for i in range(num_of_enemies):
+	for i in range(num_of_bananas):
 		
 	#Game over
-		if enemyY[i] > 200:
-			for j in range(num_of_enemies):
-				enemyY[j] = 2000
+		if bananaY[i] > 600:
+			for j in range(num_of_bananas):
+				bananaY[j] = 2000
 			game_over_text()
 			break
 
-		if enemyY[i] <= 736:
-			enemyY_change[i] = 1
-			enemyY[i] += enemyY_change[i]
-		# Collision
-		collision = isCollision(enemyX[i], enemyY[i], playerX, playerY)
-		if collision:
-			enemyX[i] = random.randint(0, 735)
-			enemyY[i] = 0
-			score_value += 1
-		enemy(enemyX[i], enemyY[i], i)
+		if bananaY[i] <= 600:
+			bananaY_change[i] = random.randint(1, 5)
+			bananaY[i] += bananaY_change[i]
 
+		# Collision
+		collision = isCollision(bananaX[i], bananaY[i], playerX, playerY)
+		if collision:
+			bananaX[i] = random.randint(0, 735)
+			bananaY[i] = 0
+			score_value += 1
+		banana(bananaX[i], bananaY[i], i)
 
 
 	player(playerX, playerY)
